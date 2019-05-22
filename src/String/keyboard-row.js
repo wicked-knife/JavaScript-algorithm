2ddc62dc48ce40d7e41b979ac4f1c242fc8d326c
fenggang <1353640486@qq.com>
2019-05-22 11:24:26 +0800

    优化键盘行算法

--- src/String/keyboard-row.js
+++ src/String/keyboard-row.js
@@ -5,27 +5,18 @@
 var findWords = function(words) {
   let res = []
   const keyboards = [
-    /[qwertyuiop]/i,
-    /[asdfghjkl]/i,
-    /[zxcvbnm]/i
+    /^[qwertyuiop]+$/i,
+    /^[asdfghjkl]+$/i,
+    /^[zxcvbnm]+$/i
   ]
   for(let i = 0; i < words.length; i++) {
     let regIndex = 0
-    let __passedFlag = 0
     for(let k = 0; k < keyboards.length; k++) {
       if( words[i][0].match( keyboards[k] ) ) {regIndex = k}
     }
-    for(let j = 1; j < words[i].length; j++) {
-      if( words[i][j].match(keyboards[regIndex]) ) {
-        __passedFlag++
-      } else {
-        break
-      }
-    }
-    if(__passedFlag === words[i].length - 1) {res.push(words[i])}
+    if(words[i].match(keyboards[regIndex])) {res.push(words[i])}
   }
   return res
 }
-
 
 export default findWords
