3182394e1e197b6acef719a32c99e5777cf4f0a8
冯罡 <1353640486@qq.com>
2019-05-24 21:42:49 +0800

    两数之和

---
+++ src/Array/two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let map = {}
+  for(let i = 0; i < nums.length; i++) {
+    let result = target - nums[i]
+    if(map[result] !== undefined) {
+      return [map[result], i]
+    }
+    map[nums[i]] = i
+  }
+}
+
+export default twoSum
---
+++ test/two-sum.test.js
@@ -0,0 +1,5 @@
+import twoSum from '../src/Array/two-sum.js'
+
+test('case 1', () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
+})
