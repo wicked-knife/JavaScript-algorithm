cf9005f0dfc5921d35f33147ac2fd208834d901e
冯罡 <1353640486@qq.com>
2019-05-27 23:38:49 +0800

    optimize

--- src/Array/kth-largest-element-in-an-array.js
+++ src/Array/kth-largest-element-in-an-array.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-  nums = nums.sort((a,b) => b - a )
-  return nums[k - 1]
+  const pivotIndex = Math.floor(Math.random() * nums.length)
+  const pivot = nums[pivotIndex]
+  const l = []
+  const r = []
+  for(let i = 0; i < nums.length; i++) {
+    nums[i] <= pivot ? l.push(nums[i]) : r.push(nums[i])
+  }
+  if(r.length === k) {
+    return r[0]
+  }
+  if(r.length > k) {
+    return findKthLargest(r, k)
+  }
+  if(k > r.length) {
+    return findKthLargest(l, k - r.length)
+  }
 }
 export default findKthLargest
--- test/kth-largest-element-in-an-array.test.js
+++ test/kth-largest-element-in-an-array.test.js
@@ -3,3 +3,11 @@ import findKthLargest from '../src/Array/kth-largest-element-in-an-array.js'
 test('case 1', () => {
   expect(findKthLargest([3,2,1,5,6,4], 2)).toBe(5)
 })
+
+test('case 2', () => {
+  expect(findKthLargest([99,99], 1)).toBe(99)
+})
+
+test('case 3', () => {
+  expect(findKthLargest([2,2,3,5], 3)).toBe(2)
+})
