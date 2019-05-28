e3ac4d6b513bd4508cdaa65a5c8af3dabf608cff
fenggang <1353640486@qq.com>
2019-05-28 09:55:13 +0800

    optimize

--- src/Array/kth-largest-element-in-an-array.js
+++ src/Array/kth-largest-element-in-an-array.js
@@ -9,16 +9,20 @@ var findKthLargest = function(nums, k) {
   const l = []
   const r = []
   for(let i = 0; i < nums.length; i++) {
-    nums[i] <= pivot ? l.push(nums[i]) : r.push(nums[i])
+    if(nums[i] > pivot) {
+      r.push(nums[i])
+      continue
+    }
+    if(nums[i] < pivot) {
+      l.push(nums[i])
+    }
   }
-  if(r.length === k) {
-    return r[0]
-  }
-  if(r.length > k) {
+  if(r.length >= k) {
     return findKthLargest(r, k)
   }
-  if(k > r.length) {
-    return findKthLargest(l, k - r.length)
+  if(k > nums.length - l.length) {
+    return findKthLargest(l, k - (nums.length - l.length))
   }
+  return pivot
 }
 export default findKthLargest
